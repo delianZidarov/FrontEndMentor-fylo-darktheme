e7b35e794954e4635485a1ab8b3a35575d5d6afd
Delian Zidarov <d.zidarov@gmail.com>
2022-04-21 18:35:35 +0900

    did the testimonial component

---
+++ src/components/Testimonials.svelte
@@ -0,0 +1,80 @@
+<style>
+    p{
+        font-size: .625rem;
+        line-height: 1.125rem;
+        margin-bottom: 1rem;
+    }
+    .testimonials-container{
+        position:relative;
+        display:flex;
+        flex-wrap: wrap;
+        gap: 1.5rem;
+        margin-left: calc(var(--body-margin) * 2);
+        margin-right: calc(var(--body-margin) * 2);
+    }
+    .testimonial{
+        padding: 1.5rem 1.25rem 1.3rem 1.25rem ;
+        z-index: 1;
+        border-radius: 4px;
+        box-shadow: 0px 0px 8px 4px rgba(56, 56, 56, 0.0458843);
+        background-color: var(--testimonials-bg);
+    }
+    .user-information-container{
+        display: grid;
+        grid-template-columns: 1.5rem auto;
+        grid-template-rows: auto auto;
+        gap: .5rem;
+    }
+    .user-description{
+        font-size: .5em;
+        line-height: .72em; 
+    }
+     h2{
+        font-size: .625rem;
+        line-height: .75rem;
+    }
+    .profile-pic{
+        width: 1.5rem;
+        height: 1.5rem;
+        border-radius: 50%;
+        grid-row:span 2;
+    }
+    .bg-quotes{
+        position:absolute;
+        width: 2rem;
+        top:-22px;
+        left:4px;
+    }
+
+</style>
+
+<section class="testimonials-container">
+    <img src="./images/bg-quotes.png" class="bg-quotes">
+    <div class="testimonial">
+        <p>  Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has 
+  become a well-oiled collaboration machine.</p>
+        <div class="user-information-container">
+            <img src="./images/profile-1.jpg" alt="Satish Pate" class="profile-pic"/>
+            <h2> Satish Pate</h2>
+            <p class="user-description"> Founder & CEO, Huddle</p>
+        </div>
+    </div>
+    <div class="testimonial">
+        <p>  Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has 
+  become a well-oiled collaboration machine.</p>
+        <div class="user-information-container">
+            <img src="./images/profile-2.jpg" alt="Bruce Mckenzie" class="profile-pic"/>
+            <h2>Bruce McKenzie</h2>
+            <p class="user-description">Founder & CEO, Huddle</p>
+        </div>
+    </div>
+    <div class="testimonial">
+        <p>  Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has 
+  become a well-oiled collaboration machine.</p>
+        <div class="user-information-container">
+            <img src="./images/profile-3.jpg" alt="Iva Boy" class="profile-pic"/>
+            <h2>Iva Boy</h2>
+            <p class="user-description">Founder & CEO, Huddle</p>
+        </div>
+    </div>
+</section>
